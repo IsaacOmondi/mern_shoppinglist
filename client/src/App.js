6c45f9071d7f64b35ccb139ae1a410175b9a85d7
isaacomondi <iomondi90@gmail.com>
2019-03-06 16:41:49 +0300

    Add redux implementation in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import AppNavBar from './components/AppNavBar';
-import ShoppingList from './components/shoppingList';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React, { Component } from "react";
 
+import AppNavBar from "./components/AppNavBar";
+import ShoppingList from "./components/shoppingList";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar/>
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
